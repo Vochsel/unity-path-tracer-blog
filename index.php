7ec50ba27d1109b3c7ecb2a6a544f77207fdee5a
Ben Skinner <benjamin.skinner96@gmail.com>
2017-10-19 10:14:40 +1100

    First post

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title>Unity Path Tracer Blog</title>
+
+        <!-- External Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+        <!-- Style Sheets -->
+        <link rel="stylesheet" type="text/css" href="css/main.css"/>
+
+    </head>
+
+    <body>
+        <div id="container">
+            <div id="header">
+                <h1 class="banner">Unity Path Tracer</h1>
+                <h3>Ben Skinner</h3>
+            </div>
+        
+            <div id="content">
+                    <?php require('posts/Entry_01.html'); ?>
+                <hr/>
+            </div>
+        </div>
+    </body>
+</html>
